1b5b17c9d89e9a98211df023353590c2e89f64c9
N. vG <nils.vangestel@gmail.com>
2024-07-21 17:30:22 +0200

    setup router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import MembersView from "../views/MembersView.vue";
+import SportsView from "../views/SportsView.vue";
+import MemberView from "../views/MemberView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
+    { path: "/", name: "home", component: HomeView },
+    { path: "/sports", name: "sports", component: SportsView },
+    { path: "/members", name: "members", component: MembersView },
+    { path: "/members/:id", name: "member", component: MemberView },
   ],
 });
 
---
+++ src/views/SportsView.vue
@@ -0,0 +1,1 @@
+<template>Sports</template>
