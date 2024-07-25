88c7b72339a80ed0c74d7aec743513a395f2ad31
N. vG <nils.vangestel@gmail.com>
2024-07-25 23:09:29 +0200

    start hover

--- src/components/HoverEffect.vue
+++ src/components/HoverEffect.vue
@@ -1,61 +1,100 @@
 <template>
-  <div class="wrapper">
-    <div class="img-parent">
-      <div class="img1"></div>
-      <div class="img2"></div>
-      <div class="img3"></div>
-      <div class="img4"></div>
-      <div class="img5"></div>
-      <div class="img6"></div>
-      <div class="img7"></div>
-      <div class="img8"></div>
-      <div class="img9"></div>
-      <div class="img10"></div>
-      <div class="img11"></div>
-      <div class="img12"></div>
-      <div class="img13"></div>
+  <body>
+    <div class="wrapper">
+      <div class="imgs-parent">
+        <div class="img1"></div>
+        <div class="img2"></div>
+        <div class="img3"></div>
+        <div class="img4"></div>
+        <div class="img5"></div>
+        <div class="img6"></div>
+        <div class="img7"></div>
+        <div class="img8"></div>
+        <div class="img9"></div>
+        <div class="img10"></div>
+        <div class="img11"></div>
+        <div class="img12"></div>
+        <div class="img13"></div>
+      </div>
     </div>
-  </div>
+  </body>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background: linear-gradient(90deg, black, #333);
+}
+
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.imgs-parent {
+  display: flex;
+  gap: 0.4rem;
+  perspective: 60rem;
+
+  div {
+    width: 5.4rem;
+    height: 21rem;
+    background-size: cover;
+    background-position: center;
+    cursor: pointer;
+    transition: transform 1s ease;
+  }
+  div:hover {
+    transform: translateZ(17.375rem);
+  }
+}
+
 .img1 {
-  background-image: voetbal;
+  background-image: url("../assets/images/soccer.png");
 }
 .img2 {
-  background-image: basketbal;
+  background-image: url("../assets/images/americanFootball.png");
 }
 .img3 {
-  background-image: climbing;
+  background-image: url("../assets/images/basketbal.png");
 }
 .img4 {
-  background-image: GoKarts;
+  background-image: url("../assets/images/boogschieten.png");
 }
 .img5 {
-  background-image: sailing;
+  background-image: url("../assets/images/cricket.png");
 }
 .img6 {
-  background-image: baseball;
+  background-image: url("../assets/images/honkbal.png");
 }
 .img7 {
-  background-image: Kickboxing;
+  background-image: url("../assets/images/ijshockey.png");
 }
 .img8 {
-  background-image: tennis;
+  background-image: url("../assets/images/karten.png");
 }
 .img9 {
-  background-image: ijshockey;
+  background-image: url("../assets/images/kickboxing.png");
 }
 .img10 {
-  background-image: american football;
+  background-image: url("../assets/images/rotsklimmen.png");
 }
 .img11 {
-  background-image: cricket;
+  background-image: url("../assets/images/sailing.png");
 }
 .img12 {
-  background-image: boogschieten;
+  background-image: url("../assets/images/tennis.png");
 }
 .img13 {
-  background-image: speerwerpen;
+  background-image: url("../assets/images/speerwerpen.png");
 }
 </style>
+
+<!-- https://www.youtube.com/watch?v=Ass8RNXVFQM&ab_channel=CodeDesign -->
