135a70990ebc618fe7bd7ef21a9a7ed90d710944
Nils van Gestel <nvangestel@yellowtail.nl>
2024-07-26 11:58:07 +0200

    table start

--- src/views/SportsView.vue
+++ src/views/SportsView.vue
@@ -1,1 +1,32 @@
-<template>Sports</template>
+<template>
+  <div class="container">
+    <h2>Sports</h2>
+    <table>
+      <tr>
+        <th>Sport</th>
+        <th>#Members</th>
+        <th>GoToMembers</th>
+        <th>Edit sport</th>
+        <th>Delete sport</th>
+      </tr>
+      <tr>
+        <td>Tennis</td>
+        <td>45</td>
+        <td>LINK</td>
+        <td><button>Edit</button></td>
+        <td><button>Delete</button></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  height: 60vh;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+</style>
